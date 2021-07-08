1c200356e1e1a354b81254c6428427b3a2fc076a
viktorpy <ruzaikinviktor@gmail.com>
2021-07-08 21:51:41 +0800

    ..

--- geoshop/mainapp/views.py
+++ geoshop/mainapp/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    links_menu = [
+        {'href': 'products_tacheometrs', 'name': 'тахеометры'},
+        {'href': 'products_gnss_equipmant', 'name': 'гнсс оборудование'},
+        {'href': 'products_3D_scanner', 'name': '3D сканеры'},
+        {'href': 'products_radio_station', 'name': 'Радиостанции'},
+        {'href': 'products_laser_rangefinder', 'name': 'Лазерные дальномерыы'},
+    ]
+    context = {
+        'links_menu': links_menu,
+    }
+    return render(request=request, template_name='mainapp/products.html', context=context)
